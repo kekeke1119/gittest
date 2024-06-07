d8cfa973eb1dc373335fe953e94ff12ed067b461
kekeke <851093421@qq.com>
2024-06-07 15:19:41 +0800

    测试远程库

--- src/com/atjum/git/GitTest.java
+++ src/com/atjum/git/GitTest.java
@@ -5,6 +5,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hot-fix 在修改");
         System.out.println("合并冲突测试 结束");
+        System.out.println("测试远程库");
     }
-
 }
