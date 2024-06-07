5c79270ce5566c2b97486969085f97a539eb7807
kekeke <851093421@qq.com>
2024-06-07 14:53:42 +0800

    合并冲突测试 master commit

--- src/com/atjum/git/GitTest.java
+++ src/com/atjum/git/GitTest.java
@@ -5,6 +5,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hot-fix 在修改");
         System.out.println("hot-fix 在修");
+        System.out.println("合并冲突测试 master");
     }
 
 }
