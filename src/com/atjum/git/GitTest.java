ad00b9c708ff8144177ae3dc6369bd4d09d2a226
kekeke <851093421@qq.com>
2024-06-07 14:42:18 +0800

    (merge test)hot-fix commit2

--- src/com/atjum/git/GitTest.java
+++ src/com/atjum/git/GitTest.java
@@ -4,6 +4,7 @@ package com.atjum.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hot-fix 在修改");
+        System.out.println("hot-fix 在修");
     }
 
 }
